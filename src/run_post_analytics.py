fc2da9731d4349aff5fd1b1ca1ff4c531ce081cc
@tinusfa <tinusfa@stud.ntnu.no>
2025-05-21 17:28:41 +0200

    more results

--- src/run_post_analytics.py
+++ src/run_post_analytics.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         os.path.join(SINGLE_RUNS_FOLDER, folder_name)
         for folder_name in single_runs_folder_names
     ]
-    batch_runs_folder_name = "batch_128_4_4_4core_bat_and_no_bat"
+    batch_runs_folder_name = "batch_128_4_batandnobat_sens_limit"
     batch_runs_folder_path = os.path.join(BATCH_RUNS_FOLDER, batch_runs_folder_name)
 
     ### select either single or batch runs
